8da5f6fda6408aa17f21280de1ce5b6b9096beb9
Jack Linke <jacklinke@gmail.com>
2022-12-20 00:50:10 -0500

    Move add_to_class for the M2M to ready

--- flexible_list_of_values/apps.py
+++ flexible_list_of_values/apps.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.db import models
 from django.apps import AppConfig
 
 logger = logging.getLogger("flexible_list_of_values")
@@ -15,6 +16,17 @@ class FlexibleListOfValuesAppConfig(AppConfig):
 
         try:
             for model in AbstractLOVValue.get_concrete_subclasses():
+                model.add_to_class(
+                    "lov_associated_entities",
+                    models.ManyToManyField(
+                        model.lov_entity_model,
+                        through=model.lov_selections_model,
+                        through_fields=("lov_value", "lov_entity"),
+                        related_name=model.lov_associated_entities_related_name,
+                        related_query_name=model.lov_associated_entities_related_query_name,
+                    ),
+                )
+
                 model.objects._import_defaults()
         except Exception as e:
             logger.error(e)
