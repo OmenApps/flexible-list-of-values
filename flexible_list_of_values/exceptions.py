519c1e5e26bf7cb8c7c65d8c5105cdbcb26e72e0
Jack Linke <jacklinke@gmail.com>
2022-12-19 19:58:07 -0500

    Add exception for cases where entity is not provided from view to form

--- flexible_list_of_values/exceptions.py
+++ flexible_list_of_values/exceptions.py
@@ -1,2 +1,19 @@
 class ModelClassParsingError(Exception):
+    """
+    Used when a model class cannot be parsed from the value provided
+    """
+    pass
+
+
+class IncorrectSubclassError(Exception):
+    """
+    Used when the value of `lov_value_model` is not a subclass of the correct abstract model
+    """
+    pass
+
+
+class NoEntityProvidedFromViewError(Exception):
+    """
+    Used when a view does not pass an lov_entity instance from the view to a form
+    """
     pass
